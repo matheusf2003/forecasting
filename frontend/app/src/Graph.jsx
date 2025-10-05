fcb03817a8230fa20d7e9d220ac5fa332b6423b1
AugustoSPorto <augusto.porto.pro@gmail.com>
2025-10-05 20:54:28 -0300

    graph css created

---
+++ frontend/app/src/Graph.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+//import Header from './components/Layout/Header';
+import MapPicker from './components/Map/MapPicker';
+import './Graph.css';
+
+import 
+
+function Graph() {
+
+return (
+<div className="container-graphs">
+  <div className="container-graphs2">
+    <h3> Cloud Coverage</h3>
+    <img src={clouds} alt="Imagem 1" />
+    <p>Texto 1</p>
+
+    <img src={humidity} alt="Imagem 2" />
+    <p>Texto 2</p>
+
+
+  </div>
+</div>
+
+)
+}
+
+export default Graph;
