52be28fd845fa0ffb9bd1d1f528eee222deb8a55
Matheus Melo <mth.melo@hotmail.com>
2025-10-05 16:05:53 -0300

    b

--- frontend/app/src/main.jsx
+++ frontend/app/src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import 'leaflet/dist/leaflet.css'
 import App from './App.jsx'
+import Header from './components/Layout/Header';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header />
     <App />
   </StrictMode>,
 )
